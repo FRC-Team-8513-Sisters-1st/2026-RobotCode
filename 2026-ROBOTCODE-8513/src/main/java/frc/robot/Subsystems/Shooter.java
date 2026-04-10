ca64033aa502dc22a1847947ee45775b7a665cb5
Owen Busler <owen1050@gmail.com>
2026-04-10 00:18:37 -0400

    lost }

--- 2026-ROBOTCODE-8513/src/main/java/frc/robot/Subsystems/Shooter.java
+++ 2026-ROBOTCODE-8513/src/main/java/frc/robot/Subsystems/Shooter.java
@@ -111,12 +111,13 @@ public class Shooter {
             }
 
         } else if (shooterState == ShooterStates.stationary && useInternalController == true) {
-            if(Robot.teleop.copilotJoystick2.getRawButton(10) & Robot.teleop.copilotJoystick2.getRawButton(11)){
+            if (Robot.teleop.copilotJoystick2.getRawButton(10) & Robot.teleop.copilotJoystick2.getRawButton(11)) {
                 setCurrentLimits(100, 20);
                 shooterMotorLeftLeader.setControl(m_request.withVelocity(-35).withFeedForward(RPStoVoltage(-35)));
             } else {
-            shooterMotorLeftLeader.set(0);
+                shooterMotorLeftLeader.set(0);
 
+            }
         }
 
         setHoodAngle();
